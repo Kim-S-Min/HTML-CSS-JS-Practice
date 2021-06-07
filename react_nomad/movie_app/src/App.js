08032c8deef8e3cfa4f734009f5d366291bb839d
김상민 <wrt123123123@gmail.com>
2021-06-07 17:22:03 +0900

    nomad movie_(web component, api, axios, async await, render)

--- react_nomad/movie_app/src/App.js
+++ react_nomad/movie_app/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -7,18 +9,43 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async() => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json";)
+    const {
+      data: { 
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false })
   }
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {this.state.isLoading ? "Loading...." : "We are ready"}
-      </div>
+      <section className="container">
+        {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id} 
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
