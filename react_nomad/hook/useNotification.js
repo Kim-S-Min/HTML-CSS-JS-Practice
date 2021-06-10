78835cfa473935bd7c11312ec9be8e89b5c2aea0
김상민 <wrt123123123@gmail.com>
2021-06-10 15:01:02 +0900

    useNotifiation

---
+++ react_nomad/hook/useNotification.js
@@ -0,0 +1,30 @@
+const useNotification = (title, options) => {
+    if (!("Notification" in window)) {
+      return;
+    }
+    const fireNotif = () => {
+      if (Notification.permission !== "granted") {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            new Notification(title, options);
+          } else {
+            return;
+          }
+        });
+      } else {
+        new Notification(title, options);
+      }
+      return fireNotif;
+    };
+  };
+  
+  const App = () => {
+    const triggerNotif = useNotification("Can i steel your kimchi?", {
+      body: "I love kimchi don't you"
+    });
+    return (
+      <div className="App" style={{ height: "1000vh" }}>
+        <button onClick={triggerNotif}>Hi</button>
+      </div>
+    );
+  };
