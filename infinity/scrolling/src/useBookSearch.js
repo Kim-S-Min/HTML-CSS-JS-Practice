d2017abef7463f99e832afd5086a9a0fcc85cbfb
김상민 <wrt123123123@gmail.com>
2021-06-11 11:15:04 +0900

    infinite scroll example

--- infinity/scrolling/src/useBookSearch.js
+++ infinity/scrolling/src/useBookSearch.js
@@ -3,14 +3,35 @@ import axios from 'axios'
 
 
 export default function useBookSearch(query, pageNumber) {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [books, setBooks] = useState([])
+    const [hasMore, setHasMore] = useState(false)
+
     useEffect(() => {
-        axios ({
+        setBooks([])
+    }, [query])
+
+    useEffect(() => {
+        setLoading(true)
+        setError(false)
+        let cancel
+        axios({
             method: 'GET',
-            url: '',
-            params: { q: query, page: pageNumber }
+            url: 'http://openlibrary.org/search.json',
+            params: { q: query, page: pageNumber },
+            cancelToken: new axios.CancelToken(c => cancel = c)
         }).then(res => {
-            console.log(res.data)
+            setBooks(prevBooks => {
+                return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+            }) 
+            setHasMore(res.data.docs.length > 0)
+            setLoading(false)
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+            setError(true)
         })
+        return () => cancel()
     }, [query, pageNumber])
-    return null
+    return { loading, error, books, hasMore }
 }
